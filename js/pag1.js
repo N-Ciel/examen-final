f549ec440f387d3242cb1ade70178c6692d4b6cf
N-Ciel <nehemiascorine@gmail.com>
2026-06-10 13:41:04 -0600

    Add files via upload

---
+++ js/pag1.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const botonEnviar = document.getElementById("btnEnviar");
+    const mensajeAviso = document.getElementById("mensajeAviso");
+    const formulario = document.getElementById("miFormulario");
+    botonEnviar.addEventListener("click", function() {
+        const todosLosInputs = formulario.querySelectorAll("input");
+        let estaVacio = false;
+        todosLosInputs.forEach(function(input) {
+            if (input.value.trim() === "") {
+                estaVacio = true; 
+            }
+        });
+        if (estaVacio === true) {
+            mensajeAviso.textContent = "Falta llenar campos!!!";
+            mensajeAviso.style.color = "red";
+        } else {
+            mensajeAviso.textContent = "GRACIAS, POR LLENAR!!!";
+            mensajeAviso.style.color = "green";
+        }
+    });
+});
